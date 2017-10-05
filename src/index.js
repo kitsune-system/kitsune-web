68f0f0a9c773217a977f9b92e9613bbbd60230d9
Edward Nicholes Jr <edward.nicholes@target.com>
2017-10-04 21:52:36 -0500

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './app';
+import App from './app/app';
 
 import './index.scss';
+
+console.log('Hello Kitsune');
 
 render(<App/>, document.getElementById('root'));
 
 if(module.hot) {
-  module.hot.accept('./app', () => {
-    const App = require('./app').default;
+  module.hot.accept('./app/app', () => {
+    const App = require('./app/app').default;
     render(<App/>, document.getElementById('root'));
   });
 }
