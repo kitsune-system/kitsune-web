ef5ad150bfd8e54b0e874c53b9c08cc93c150066
Edward Nicholes Jr <GameDevFox@gmail.com>
2019-03-12 22:24:32 -0700

    Add command list

--- src/kitsune/console.js
+++ src/kitsune/console.js
@@ -28,6 +28,7 @@ const service = KitsuneService(request);
 // TODO: Use hooks
 function Console() {
   const [random, setRandom] = useState('');
+  const [commandList, setCommandList] = useState([]);
 
   const onRandomClick = () => {
     service('ijJv0As7V8Vk8kx1kL5Rm+LSDyHnfFPazUVtB/pmZiw=').then(result => {
@@ -37,6 +38,10 @@ function Console() {
   };
 
   useEffect(onRandomClick, []);
+
+  useEffect(() => {
+    service('commands').then(setCommandList);
+  }, []);
 
   const button = (<Button onClick={onRandomClick}>Random</Button>);
 
@@ -48,6 +53,10 @@ function Console() {
         <Input type="text" placeholder="Tail"/>
         <Button>Create</Button>
       </div>
+      <div>List Commands</div>
+      <ul>
+        {Object.entries(commandList).map(([hash, name]) => <li><Input readonly type="text" value={hash}/> - {JSON.stringify(name)}</li>)}
+      </ul>
     </div>
   );
 }
