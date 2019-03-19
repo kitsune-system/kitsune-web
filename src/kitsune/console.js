38d665ceab4dc37e308c1c62ff275086a73fd5b2
Edward Nicholes Jr <GameDevFox@gmail.com>
2019-03-18 20:31:14 -0700

    Front-end for 'writeEdge' (with Save)

--- src/kitsune/console.js
+++ src/kitsune/console.js
@@ -36,12 +36,21 @@ const CopyInput = props => {
   return <Input ref={inputEl} action={action} {...props}/>;
 };
 
+const Node = props => <CopyInput readOnly type="text" {...props}/>;
+
+const bind = statePair => ({
+  onChange: e => statePair[1](e.currentTarget.value),
+  value: statePair[0],
+});
+
 function Console() {
   const [random, setRandom] = useState('');
   const [commandList, setCommandList] = useState([]);
   const [edgeList, setEdgeList] = useState([]);
+  const [head, tail] = ['', ''].map(useState);
 
   const onRandomClick = () => service.random().then(setRandom);
+  const onWriteEdgeClick = () => service('/writeEdge', [head[0], tail[0]]);
 
   useEffect(() => {
     onRandomClick();
@@ -51,27 +60,31 @@ function Console() {
   }, []);
 
   const commands = Object.entries(commandList).map(([hash, name]) => {
-    return <li key={hash}><CopyInput readOnly type="text" value={hash}/> - {JSON.stringify(name)}</li>;
+    return <li key={hash}><Node value={hash}/> - {JSON.stringify(name)}</li>;
   });
 
   const edges = edgeList.map(([head, tail, id]) => (
-    <li key={id}>{head} {'>>>'} {tail} # {id}</li>
+    <li key={id}><Button icon="close"/><Node value={head}/> {'->'} <Node value={tail}/> # <Node value={id}/></li>
   ));
 
   return (
     <div>
+      <Button onClick={() => service('/save')}>Save</Button>
+      <Button onClick={() => service('/load')}>Load</Button>
       <div style={{ display: 'flex', width: '100%' }}>
         <Button onClick={onRandomClick}>Random</Button>
-        <CopyInput style={{ flexGrow: 1 }} type="text" value={random}/>
+        <Node style={{ flexGrow: 1 }} value={random}/>
       </div>
       <div>
-        <Input type="text" placeholder="Head"/>
-        <Input type="text" placeholder="Tail"/>
-        <Button>Create</Button>
+        <CopyInput type="text" placeholder="Head" {...bind(head)}/>
+        <CopyInput type="text" placeholder="Tail" {...bind(tail)}/>
+        <Button onClick={onWriteEdgeClick}>Write Edge</Button>
       </div>
-      <div>Commands:</div>
+
+      <h2>Commands:</h2>
       <ul>{commands}</ul>
-      <div>Edges:</div>
+
+      <h2>Edges:</h2>
       <ul>{edges}</ul>
     </div>
   );
