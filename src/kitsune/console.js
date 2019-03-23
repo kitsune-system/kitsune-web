41e599d8f5e5155f8420a34c9d5014ea0cb7053e
Edward Nicholes Jr <GameDevFox@gmail.com>
2019-03-22 22:41:00 -0700

    Add tabs

--- src/kitsune/console.js
+++ src/kitsune/console.js
@@ -1,7 +1,6 @@
-/* eslint-disable */
 import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
-import { Button, Grid, Input } from 'semantic-ui-react';
+import { Button, Input, List, Tab } from 'semantic-ui-react';
 
 import KitsuneService from './kitsune-service';
 
@@ -29,7 +28,7 @@ const CopyInput = props => {
 
   const copy = () => {
     inputEl.current.select();
-    document.execCommand("copy");
+    document.execCommand('copy');
   };
 
   const action = <Button icon="copy outline" onClick={copy}/>;
@@ -50,74 +49,126 @@ const mapState = (...initialState) => {
   });
 };
 
-function Console() {
-  const [random, setRandom] = useState('');
+const CommandList = () => {
   const [commandList, setCommandList] = useState([]);
-  const [edgeList, stringList] = mapState([], []);
-  const [head, tail] = mapState('', '');
-  const [string] = mapState('');
-
-  const onRandomClick = () => service.random().then(setRandom);
-
-  const onWriteEdgeClick = () => {
-    service('/writeEdge', [head(), tail()]).then(() => {
-      head(''); tail('');
-    });
-  };
-
-  const onWriteStringClick = () => {
-    service('/writeString', string()).then(hash => string(hash));
-  };
 
   useEffect(() => {
-    onRandomClick();
-
     service('commands').then(setCommandList);
-    service('listEdge').then(edgeList);
-    service('listString').then(stringList);
   }, []);
 
   const commands = Object.entries(commandList).map(([hash, name]) => {
-    return <li key={hash}><Node value={hash}/> - {JSON.stringify(name)}</li>;
+    return <List.Item key={hash}><Node value={hash}/> {JSON.stringify(name)}</List.Item>;
   });
 
-  const edges = edgeList().map(([head, tail, id]) => (
-    <li key={id}><Button icon="close"/><Node value={head}/> {'->'} <Node value={tail}/> # <Node value={id}/></li>
-  ));
+  return <Tab.Pane><List items={commands}/></Tab.Pane>;
+};
 
-  const strings = stringList().map(({ id, string }) => (
-    <li key={id}><Button icon="close"/><Node value={id}/><Node value={string}/></li>
+const EdgeList = () => {
+  const [edgeList] = mapState([]);
+  const [head, tail] = mapState('', '');
+
+  useEffect(() => {
+    service('listEdge').then(edgeList);
+  }, []);
+
+  const onWriteEdgeClick = () => {
+    service('/writeEdge', [head(), tail()]).then(() => {
+      head('');
+      tail('');
+    });
+  };
+
+  const edges = edgeList().map(([head, tail, id]) => (
+    <div key={id}>
+      <Button icon="close"/>
+      <Node value={id}/>
+      <Node value={head}/>
+      <Node value={tail}/>
+    </div>
   ));
 
   return (
-    <div>
-      <Button onClick={() => service('/save')}>Save</Button>
-      <Button onClick={() => service('/load')}>Load</Button>
-      <div style={{ display: 'flex', width: '100%' }}>
-        <Button onClick={onRandomClick}>Random</Button>
-        <Node style={{ flexGrow: 1 }} value={random}/>
-      </div>
+    <Tab.Pane>
       <div>
         <CopyInput type="text" placeholder="Head" {...bind(head)}/>
         <CopyInput type="text" placeholder="Tail" {...bind(tail)}/>
         <Button onClick={onWriteEdgeClick}>Write Edge</Button>
       </div>
 
-      <div>
-        <CopyInput type="text" placeholder="String" {...bind(string)}/>
-        <Button onClick={onWriteStringClick}>Write String</Button>
-      </div>
-
-      <h2>Commands:</h2>
-      <ul>{commands}</ul>
-
-      <h2>Edges:</h2>
-      <ul>{edges}</ul>
-
-      <h2>Strings:</h2>
-      <ul>{strings}</ul>
-    </div>
+      <List items={edges}/>
+    </Tab.Pane>
   );
-}
+};
+
+const StringList = () => {
+  const [string, stringList] = mapState('', []);
+
+  useEffect(() => {
+    service('listString').then(stringList);
+  }, []);
+
+  const onWriteStringClick = () => {
+    service('/writeString', string()).then(hash => string(hash));
+  };
+
+  const strings = stringList().map(({ id, string }) => (
+    <div key={id}><Button icon="close"/><Node value={id}/><Node value={string}/></div>
+  ));
+
+  return (
+    <Tab.Pane>
+      <CopyInput type="text" placeholder="String" {...bind(string)}/>
+      <Button onClick={onWriteStringClick}>Write String</Button>
+
+      <List items={strings}/>
+    </Tab.Pane>
+  );
+};
+
+import styled from 'styled-components';
+const Flex = styled.div`{
+  display: flex;
+
+  & > * {
+    flex-grow: 1;
+  }
+}`;
+
+const flexGrow = (val = 1) => ({ style: { flexGrow: val } });
+const noGrow = flexGrow(0);
+
+const Console = () => {
+  const [random, setRandom] = useState('');
+
+  const onRandomClick = () => service.random().then(setRandom);
+
+  useEffect(() => {
+    onRandomClick();
+  }, []);
+
+  return (
+    <List>
+      <List.Item>
+        <Button onClick={() => service('/save')}>Save</Button>
+        <Button onClick={() => service('/load')}>Load</Button>
+      </List.Item>
+
+      <List.Item>
+        <Flex>
+          <Button {...noGrow} onClick={onRandomClick}>Random</Button>
+          <Node value={random}/>
+        </Flex>
+      </List.Item>
+
+      <List.Item>
+        <Tab panes={[
+          { menuItem: 'Commands', render: () => <CommandList/> },
+          { menuItem: 'Edges', render: () => <EdgeList/> },
+          { menuItem: 'Strings', render: () => <StringList/> },
+        ]}/>
+      </List.Item>
+    </List>
+  );
+};
 
 export default Console;
